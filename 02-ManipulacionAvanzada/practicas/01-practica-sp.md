3bba709b93b5c83ab3c511ecbc078eb3485d4362
Adonai García <garciadonai2512@gmail.com>
2026-03-26 18:11:38 -0600

    Practica venta con stock procedure

---
+++ 02-ManipulacionAvanzada/practicas/01-practica-sp.md
@@ -0,0 +1,126 @@
+# usp_agregar_ventas
+
+## Descripción general
+
+El **SP** usp_agregar_ventas tiene como objetivo registrar una venta en el sistema, mediante validaciones sobre el cliente, producto y existencia en inventario.
+
+Este procedimiento realiza operaciones en las siguientes tablas:
+
+* CatCliente -> Verificación del cliente
+* CatProducto -> Validación de existencia y obtención de 
+* precio
+* TBLVenta -> Registro de la venta
+* TBLDetalleVenta -> Registro del detalle de la venta
+
+## Lógica del procedimiento
+
+El procedimiento sigue una secuencia lógica como la de una tienda de chocolates:
+
+1. Verificación del cliente (en una tienda hay que verificar si hay algun cliente o alucinas)
+
+Se valida que el cliente exista en la tabla CatCliente.
+
+![alt text](image.png)
+
+2. Verificación del producto (Hay que ver si tenemos el chocolate que quiere el cliente)
+
+Se valida que el producto exista en la tabla CatProducto.
+
+![alt text](image-1.png)
+
+
+3. Validación de existencia (Hay que ver si aun hay de eso chocolates)
+
+Se obtiene el precio y la existencia del producto:
+
+![alt text](image-2.png)
+
+Se valida que haya suficiente inventario:
+
+![alt text](image-3.png)
+
+**Si cualquiera de estas comprobaciones falla Se lanza un error con THROW y se cancela la operación**
+
+4. Registro de la venta (Hay que registrar esto en el sistema)
+
+Se inserta la venta en la tabla TBLVenta:
+
+![alt text](image-4.png)
+
+Obtención del ID de venta
+![alt text](image-5.png)
+
+Este valor se utiliza para relacionar el detalle de la venta.
+
+5. Registro del detalle de venta (Imprimir el recibo)
+
+Se inserta el producto vendido:
+
+![alt text](image-6.png)
+
+6. Actualización de inventario (Hay que actualizar nuestros almacenes)
+
+Se descuenta la cantidad vendida:
+
+![alt text](image-7.png)
+
+7. Manejo de transacciones
+
+El procedimiento utiliza una transacción para garantizar consistencia:
+
+```
+BEGIN TRANSACTION
+COMMIT
+ROLLBACK
+```
+
+Si todo sale bien -> COMMIT
+Si ocurre un error -> ROLLBACK
+
+8. Manejo de errores
+
+Se puso un bloque TRY...CATCH:
+
+```
+BEGIN TRY
+BEGIN CATCH
+```
+
+Dentro del CATCH:
+
+Se revierte la transacción
+Se muestran mensajes de error:
+```
+ERROR_MESSAGE()
+ERROR_LINE()
+```
+9. Ejecución del procedimiento
+
+Ejemplo de uso:
+
+```
+EXEC usp_agregar_ventas
+@Id_Cliente = 'ANATR', 
+@Id_Producto = 4, 
+@Cantidad_Vendida = 5;
+```
+10. Resultado esperado
+
+Si todo es correcto:
+
+* Se registra una venta en TBLVenta
+* Se registra el detalle en TBLDetalleVenta
+* Se actualiza el inventario en CatProducto
+![alt text](image-8.png)
+![alt text](image-9.png)
+![alt text](image-10.png)
+
+## Conclusión
+
+El procedimiento usp_agregar_ventas asegura:
+
+* Validación de datos
+* Control de inventario
+* Manejo de errores
+
+Esto garantiza que el sistema sea confiable y evita registros incorrectos en la base de datos.
---
+++ 02-ManipulacionAvanzada/practicas/01-practica-sp.sql
@@ -0,0 +1,154 @@
+CREATE DATABASE bdpracticas;
+GO
+
+USE bdpracticas
+GO
+
+CREATE TABLE CatProducto
+(
+Id_producto INT IDENTITY PRIMARY KEY,
+Nombre_Producto NVARCHAR(40),
+Existencia INT,
+Precio MONEY
+);
+
+SELECT *
+FROM CatProducto
+
+CREATE TABLE CatCliente
+(
+Id_Cliente NCHAR(5) PRIMARY KEY,
+Nombre_Cliente NVARCHAR(40),
+Pais NVARCHAR(15),
+Ciudad NVARCHAR(15)
+);
+
+CREATE TABLE TBLVenta
+(
+Id_Venta INT IDENTITY PRIMARY KEY,
+Fecha DATE,
+Id_Cliente NCHAR(5),
+FOREIGN KEY (Id_Cliente) REFERENCES CatCliente(Id_Cliente)
+);
+
+CREATE TABLE TBLDetalleVenta
+(
+Id_Venta INT,
+Id_Producto INT,
+Precio_Venta MONEY,
+Cantidad_Vendida INT,
+PRIMARY KEY (Id_Venta, Id_Producto),
+FOREIGN KEY (Id_Venta) REFERENCES tblVenta(Id_Venta),
+FOREIGN KEY (Id_Producto) REFERENCES CatProducto(Id_Producto)
+);
+
+INSERT INTO CatProducto (Nombre_Producto, Existencia, Precio)
+SELECT ProductName, UnitsInStock, UnitPrice FROM NORTHWND.dbo.Products
+
+INSERT INTO CatCliente(Id_Cliente, Nombre_Cliente,Pais, Ciudad)
+SELECT CustomerID, ContactName, Country, City FROM NORTHWND.dbo.Customers
+
+SELECT *
+FROM CatProducto
+
+CREATE OR ALTER PROC usp_agregar_ventas
+    @Id_Cliente NCHAR(5),
+    @Id_Producto INT,
+    @Cantidad_Vendida INT
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION
+
+        DECLARE @Id_Venta INT;
+        DECLARE @Precio_Unitario MONEY;
+        DECLARE @Precio_Venta MONEY;
+        DECLARE @Existencia INT;
+
+        -- El cliente existe o estoy loco?
+        IF NOT EXISTS (
+            SELECT 1 FROM CatCliente 
+            WHERE Id_Cliente = @Id_Cliente
+        )
+        BEGIN
+            PRINT 'No reconozco a este tipo';
+            THROW 50001, 'Cliente no existe', 1;
+        END
+
+        PRINT 'A este tipo lo conozco';
+
+        -- Tendre eso que pidio?
+        IF NOT EXISTS (
+            SELECT 1 FROM CatProducto 
+            WHERE Id_Producto = @Id_Producto
+        )
+        BEGIN
+            PRINT 'Creo que ya no tengo, lo siento';
+            THROW 50002, 'Producto no existe', 1;
+        END
+
+        PRINT 'De pura suerte aun tengo';
+
+        -- Cuanto costara?... Aun tendre?
+        SELECT 
+            @Precio_Unitario = Precio,
+            @Existencia = Existencia
+        FROM CatProducto 
+        WHERE Id_Producto = @Id_Producto;
+
+        -- Si tendre lo que me pide?
+        IF @Existencia < @Cantidad_Vendida
+        BEGIN
+            PRINT 'No hay suficiente stock';
+            THROW 50003, 'Stock insuficiente', 1;
+        END
+
+        PRINT 'Stock suficiente';
+
+         -- Precio unitario en el ticket
+        SET @Precio_Venta = @Precio_Unitario;
+
+        -- Cuando hicimos la venta en el ticket
+        INSERT INTO TBLVenta (Fecha, Id_Cliente)
+        VALUES (GETDATE(), @Id_Cliente);
+
+        SET @Id_Venta = @@IDENTITY;
+
+        -- Insertar detalles en el ticket
+        INSERT INTO TBLDetalleVenta (Id_Venta, Id_Producto, Cantidad_Vendida, Precio_Venta)
+        VALUES (@Id_Venta, @Id_Producto, @Cantidad_Vendida, @Precio_Venta);
+
+        -- Actualizar inventario
+        UPDATE CatProducto
+        SET Existencia = Existencia - @Cantidad_Vendida
+        WHERE Id_Producto = @Id_Producto;
+
+        COMMIT;
+
+        PRINT 'Venta realizada correctamente';
+
+    END TRY
+    BEGIN CATCH
+
+        IF @@TRANCOUNT > 0
+        BEGIN
+            ROLLBACK;
+        END
+
+        PRINT 'Error en la venta';
+        PRINT ERROR_MESSAGE();
+        PRINT 'Linea: ' + CAST(ERROR_LINE() AS VARCHAR);
+
+    END CATCH
+END;
+
+EXEC usp_agregar_ventas
+@Id_Cliente = 'ANATR', 
+@Id_Producto = 4, 
+@Cantidad_Vendida = 5;
+
+SELECT *
+FROM TBLDetalleVenta
+
+SELECT *
+FROM TBLVenta
